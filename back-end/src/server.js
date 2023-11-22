3a908a6a68fe776f24d42ce7c9ecb8309f9b1c09
unknown <yenb2012172@student.ctu.edu.vn>
2023-11-23 05:37:07 +0700

    es

---
+++ back-end/src/server.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+
+
+async function start() {
+
+    const url = `mongodb+srv://ngocyen:123@cluster0.oibbwun.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(url);
+    
+    await client.connect();
+    const db = client.db('se-db');
+
+    const app = express();
+    app.use(express.json());
+
+    app.use('/images', express.static(path.join(__dirname, '../src/assets')));
+
+    app.get('/api/products', async (req, res) => {
+        const products = await db.collection('products').find({}).toArray();
+        res.send(products);
+    });
+
+    async function populateCartIds(ids) {
+        return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
+    }
+
+    app.get('/api/users/:userId/cart', async (req, res) => {
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populateCartIds(user.cartItems);
+        res.json(populatedCart);
+    });
+
+    app.get('/api/products/:productId', async (req, res) => {
+        const productId = req.params.productId;
+        const product = await db.collection('products').findOne({ id: productId });
+        res.json(product);
+    });
+
+    app.post('/api/users/:userId/cart', async (req, res) => {
+        const useId = req.params.userId;
+        const productId = req.body.id;
+
+        await db.collection('users').updateOne({ id: useId}, {
+            $addToSet: {cartItems: productId}
+        });
+
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populateCartIds(user.cartItems);
+        res.json(populatedCart);
+
+    });
+
+    app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
+        const useId = req.params.userId; 
+        const productId = req.params.productId;
+
+        await db.collection('users').updateOne({ id: useId}, {
+            $pull: {cartItems: productId},
+        });
+
+        const user = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populateCartIds(user.cartItems);
+        res.json(populatedCart);
+    })
+
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    });
+}
+
+start();
---
+++ back-end/src/temp-data.js
@@ -0,0 +1,52 @@
+// import basilBottle from './assets/iPhone14ProMax.jpg';
+// import lemonBottle from './assets/iPhone13ProMax.jpg';
+// import garlicBottle from './assets/OPPOReno5.jpg';
+// import rosemaryBottle from './assets/OPPOReno8T5G.jpg';
+// import chiliBottle from './assets/SamsungGalaxyNote20Ultra.jpg';
+// import delicateBottle from './assets/SamsungGalaxyS21Ultra5G.jpg';
+// import mediumBottle from './assets/XiaomiMi10TPro.jpg';
+// import boldBottle from './assets/XiaomiRedmiNote12Pro5G.jpg';
+
+export const cartItems =['123','345','456'];
+
+export const products = [{
+    id: '123',
+    name: 'iPhone 14 Pro Max',
+    price: '26.890.000',
+    imageUrl: '/images/iPhone14ProMax.jpg',
+}, {
+    id: '234',
+    name: 'iPhone 13 Pro Max',
+    price: '16.090.000',
+    imageUrl: '/images/iPhone13ProMax.jpg',
+}, {
+    id: '345',
+    name: 'OPPO Reno 5',
+    price: '8.690.000',
+    imageUrl: '/images/OPPOReno5.jpg',
+}, {
+    id: '456',
+    name: 'OPPO Reno 8T 5G',
+    price: '8.990.000',
+    imageUrl: '/images/OPPOReno8T5G.jpg',
+}, {
+    id: '567',
+    name: 'Samsung Galaxy Note 20U ltra',
+    price: '$32.990.000',
+    imageUrl: '/images/SamsungGalaxyNote20Ultra.jpg',
+}, {
+    id: '678',
+    name: 'Samsung Galaxy S21 Ultra 5G',
+    price: '16.050.000',
+    imageUrl: '/images/SamsungGalaxyS21Ultra5G.jpg',
+}, {
+    id: '789',
+    name: 'Xiaomi Mi 10 TPro',
+    price: '11.200.000',
+    imageUrl: '/images/XiaomiMi10TPro.jpg',
+}, {
+    id: '890',
+    name: 'Xiaomi Redmi Note 12 Pro 5G',
+    price: '8.490.000',
+    imageUrl: '/images/XiaomiRedmiNote12Pro5G.jpg',
+}];
